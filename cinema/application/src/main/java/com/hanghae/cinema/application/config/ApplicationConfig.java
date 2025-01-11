ea060d26e428d60ae95d61f1d34eff57d801a2d8
kdmstj <rkddmstj429@gmail.com>
2025-01-11 15:08:43 +0900

    chore: 컴포넌트 스캔을 위한 설정 파일 추A
    - 애플리케이션 진입접에서 다른 모듈들에 대한 빈을 탐색할 수 있도록 설정 파일을 import 하였습니다.

---
+++ cinema/application/src/main/java/com/hanghae/cinema/application/config/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.hanghae.cinema.application.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.hanghae.cinema.application")
+public class ApplicationConfig {
+}
