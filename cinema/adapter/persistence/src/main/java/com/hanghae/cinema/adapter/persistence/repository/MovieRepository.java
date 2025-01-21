7919f86ae819820372d8980809af63affdeef824
kdmstj <rkddmstj429@gmail.com>
2025-01-21 19:57:25 +0900

    fix: MovieRepository 사용하지 않는 메소드 삭제
    - queryDSL 을 통한 조회로 사용하지 않은 메소드는 삭제하였습니다.

--- cinema/adapter/persistence/src/main/java/com/hanghae/cinema/adapter/persistence/repository/MovieRepository.java
+++ cinema/adapter/persistence/src/main/java/com/hanghae/cinema/adapter/persistence/repository/MovieRepository.java
@@ -4,24 +4,12 @@ import com.hanghae.cinema.adapter.persistence.entity.MovieEntity;
 import com.hanghae.cinema.application.dto.MovieResponseDTO;
 import com.hanghae.cinema.domain.type.MovieGenre;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MovieRepository extends JpaRepository<MovieEntity, Long>, MovieRepositoryCustom {
-    @Query("""
-                    select m.id, m.title, m.thumbNail, m.rating, m.releaseAt, m.runningTime, m.genre,
-                    s.id, s.startedAt, s.endedAt, t.id, t.name
-                    from MovieEntity m
-                    join ShowTimeEntity s on m.id = s.movie.id
-                            join TheaterEntity t on t.id = s.theater.id
-                            where m.releaseAt <= :date and s.startedAt > :dateTime
-                            order by m.releaseAt DESC, s.startedAt ASC
-            """)
-    List<Object[]> findMoviesWithShowTimesAndTheaters(LocalDate date, LocalDateTime dateTime);
 
     List<MovieResponseDTO> findMovie(LocalDate date, LocalDateTime dateTime, String keyword, MovieGenre genre);
 }
