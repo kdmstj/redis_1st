af3f2ec0378b4fe58f861334f962a1d0753041a2
kdmstj <rkddmstj429@gmail.com>
2025-02-19 15:17:16 +0900

    add: redis 를 통해 캐싱 처리
    - key는 문자열로 직렬화하기 위해서 StringRedisSerializer 를 사용해주었고, value 는 json 형태로 직렬화하기 위해서 GenricJackson2JsonRedisSerializer 를 사용해주었습니다
    - 직렬화과정에서 LocalDate 타입을 직렬화하지 못하는 문제가 발생해 objectMapper 에 JavaTimeModule 를 등록해주었습니다.
    - 하루에 한번 영화를 등록한다고 하였을 때, Ttl 은 1시간 단위로 설정해주었습니다.

--- cinema/application/src/main/java/com/hanghae/cinema/application/config/CacheConfig.java
+++ cinema/application/src/main/java/com/hanghae/cinema/application/config/CacheConfig.java
@@ -1,5 +1,7 @@
 package com.hanghae.cinema.application.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.hanghae.cinema.application.CacheType;
 import org.springframework.cache.annotation.EnableCaching;
@@ -7,7 +9,12 @@ import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -37,5 +44,19 @@ public class CacheConfig {
         cacheManager.setCaches(caches);
 
         return cacheManager;
+    }
+
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        return RedisCacheConfiguration
+                .defaultCacheConfig()
+                .disableCachingNullValues()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)))
+                .entryTtl(Duration.ofHours(1));
     }
 }
